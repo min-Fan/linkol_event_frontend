518232962133a0091e920ea8c86b433a92e5496f
min-Fan <1258162171@qq.com>
2025-09-10 16:54:59 +0800

    feat✨: raffle reward card ui mobile

--- app/[locale]/market_events/[eventId]/compontents/RaffleRewardCard.tsx
+++ app/[locale]/market_events/[eventId]/compontents/RaffleRewardCard.tsx
@@ -142,11 +142,11 @@ export default function RaffleRewardCard({
             {/* {t('each_tweet_counts_raffle_entry')} */}
             {t('one_tweet_one_entry')}
           </h3>
-          <div className="flex flex-wrap items-center gap-2">
-            <p className="sm:text-md text-primary/50 bg-primary/10 rounded-full px-10 py-1 text-sm">
+          <div className="flex flex-wrap items-center justify-center gap-2">
+            <p className="sm:text-md text-primary/50 bg-primary/10 flex-1 rounded-full px-8 py-1 text-sm whitespace-nowrap">
               {t('reward_rate')}: <span className="">{rewardPercent * 100}%</span>
             </p>
-            <p className="sm:text-md text-primary/50 bg-primary/10 rounded-full px-4 py-1 text-sm">
+            <p className="sm:text-md text-primary/50 bg-primary/10 flex-1 rounded-full px-2 py-1 text-sm whitespace-nowrap">
               {t.rich('win_up_to', {
                 amount: (chunks) => <span className="text-primary font-bold">${10}</span>,
               })}
@@ -169,7 +169,7 @@ export default function RaffleRewardCard({
               onClick={handleRaffleTasks}
               size="sm"
               className={cn(
-                'bg-primary/5 border-primary/20 text-primary/50 !shadow-primary/10 hover:bg-primary/10 hover:text-primary !h-auto !rounded-full border px-2 !py-0.5 !shadow-xl ml-2'
+                'bg-primary/5 border-primary/20 text-primary/50 !shadow-primary/10 hover:bg-primary/10 hover:text-primary ml-2 !h-auto !rounded-full border px-2 !py-0.5 !shadow-xl'
               )}
             >
               {t('want_more')}
